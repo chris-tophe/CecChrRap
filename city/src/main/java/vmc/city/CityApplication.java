12047784715cd6b69f27d2223d939d6b24143647
CecileGallioz <58588057+CecileGallioz@users.noreply.github.com>
2020-04-22 15:52:23 +0200

    initialisationJeuDonnees

--- city/src/main/java/vmc/city/CityApplication.java
+++ city/src/main/java/vmc/city/CityApplication.java
@@ -19,22 +19,22 @@ public class CityApplication {
 		SpringApplication.run(CityApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner createCities(CityDao cityRepo, BuildingCityDao buildingRepo ) {
-		return args -> {
-			Faker faker = new Faker();
-			for(int i = 0; i<3; i++) {
-				City city = new City();
-				city.setName(faker.elderScrolls().region());
-				for (int j = 0; j<2;j++) {
-					BuildingCity building = new BuildingCity();
-					building.setName(faker.elderScrolls().city());
-					building.setPhotoUrl(faker.avatar().image().toString());
-					city.addBuilding(building);
-					buildingRepo.save(building);
-				}
-				cityRepo.save(city);
-			}	
-		};
-	}
+//	@Bean
+//	CommandLineRunner createCities(CityDao cityRepo, BuildingCityDao buildingRepo ) {
+//		return args -> {
+//			Faker faker = new Faker();
+//			for(int i = 0; i<3; i++) {
+//				City city = new City();
+//				city.setName(faker.elderScrolls().region());
+//				for (int j = 0; j<2;j++) {
+//					BuildingCity building = new BuildingCity();
+//					building.setName(faker.elderScrolls().city());
+//					building.setPhotoUrl(faker.avatar().image().toString());
+//					city.addBuilding(building);
+//					buildingRepo.save(building);
+//				}
+//				cityRepo.save(city);
+//			}	
+//		};
+//	}
 }
---
+++ city/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO building_city VALUES (1,'Cathédrale Notre-Dame','https://en.wikipedia.org/wiki/File:Strasbourg_Cathedral_Exterior_-_Diliff.jpg');
+INSERT INTO city VALUES (1,'Strasbourg');
+INSERT INTO city_buildings VALUES (1,1);
