19aad4cf459d0077dc7512320c206a1d1f79263f
RaphaelKoschig <52168525+RaphaelKoschig@users.noreply.github.com>
2020-04-23 12:04:43 +0200

    Updates CityUiController to be compatible with AppMainUiController
    and improves list of cities to make it selectable and get data from it

--- javafxui/src/main/java/vmc/javafxui/UiController/CityUiController.java
+++ javafxui/src/main/java/vmc/javafxui/UiController/CityUiController.java
@@ -1,57 +1,65 @@
 package vmc.javafxui.UiController;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
-import javafx.application.HostServices;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
-import javafx.stage.Stage;
 import vmc.javafxui.beans.CityBean;
-import vmc.javafxui.beans.UserBean;
-import vmc.javafxui.proxies.CityProxy;
-import vmc.javafxui.proxies.UserProxy;
 
 @Component
 @Controller
 public class CityUiController {
-
-	private final HostServices hostServices;
 	
-	@Autowired
-	private CityProxy cityProxy;
 	
 	@FXML
-	public ListView<CityBean> cities;
+	public ListView<CityBean> CityListView;
+	ObservableList<String> cities;
 	
-	public CityUiController(HostServices hostServices) {
-		this.hostServices = hostServices;
+	AppMainUiController main;
+	
+	public CityUiController() {
 	}
+	
+	public void setMainApp(AppMainUiController mainApp) {
+        this.main = mainApp;
+    }
 	
 	@FXML
 	public void initialize() {
-		for (CityBean cityBean : cityProxy.getCities()) {
-			cities.getItems().add(cityBean);
-		}
-		
-		cities.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+		// Rend les villes de la liste sélectionnables
+		CityListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 	}
 	
+	public void refresh() {
+		CityListView.getItems().clear();
+		CityListView.getItems().addAll(main.getCityList());
+		
+		// Met à jour les cellules pour n'afficher que le nom des villes
+		CityListView.setCellFactory(CityListView -> new ListCell<CityBean>() {
+			@Override
+			public void updateItem(CityBean city, boolean empty) {
+				super.updateItem(city, empty);
+				if (empty) {
+					setText(null);
+				}
+				else {
+					setText(city.getName());
+				}
+			}
+		});
+		
+	}
+	
+	// Récupère des informations d'une ville sélectionnée
 	public void cityClick(Event e) throws Exception {
 		ObservableList<CityBean> selectedCity;
-		selectedCity = cities.getSelectionModel().getSelectedItems();
-		System.out.println("You selected " + selectedCity.get(0).getName());
+		selectedCity = CityListView.getSelectionModel().getSelectedItems();
+		System.out.println("You selected city with id : " + selectedCity.get(0).getIdCity());
 	}
 	
 	
