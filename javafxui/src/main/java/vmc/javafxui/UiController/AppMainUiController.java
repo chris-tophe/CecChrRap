49d29dd218957c9b6f83894a163e05ed08a2564f
Chris-tophe <christophe.loyer@gmail.com>
2020-04-23 10:39:18 +0200

    getlistbuilding proxy

--- javafxui/src/main/java/vmc/javafxui/UiController/AppMainUiController.java
+++ javafxui/src/main/java/vmc/javafxui/UiController/AppMainUiController.java
@@ -2,19 +2,28 @@ package vmc.javafxui.UiController;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
+import com.sun.javafx.collections.ArrayListenerHelper;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import vmc.javafxui.beans.BuildingCityBean;
+import vmc.javafxui.proxies.CityProxy;
 
 
 @Component
@@ -23,9 +32,15 @@ public class AppMainUiController implements Initializable{
 	
 	Resource buildingCityUi;
 	private final ApplicationContext appContext;
-	
+		
 	@FXML
 	AnchorPane buildingCityViewPane;
+	
+	@Autowired
+	CityProxy cities;
+	
+	private List<BuildingCityBean> buildingCityList = new LinkedList<BuildingCityBean>();
+	
 	
 	public AppMainUiController(@Value("classpath:/buildingCityUi.fxml") Resource buildingCityUi, ApplicationContext appContext  ) {
 		this.buildingCityUi = buildingCityUi;
@@ -34,21 +49,27 @@ public class AppMainUiController implements Initializable{
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		VBox listViewBuildingCity;
+		VBox listViewBuildingCity = new VBox();
+		BuildingCityUiController buildingCityUiController = new BuildingCityUiController() ;
 		try {
 			FXMLLoader buildingCityUiLoader = new FXMLLoader(this.buildingCityUi.getURL());
 			buildingCityUiLoader.setControllerFactory(appContext::getBean);
 			listViewBuildingCity = (VBox) buildingCityUiLoader.load();
-			AnchorPane.setBottomAnchor(listViewBuildingCity, (double) 0);
-			AnchorPane.setTopAnchor(listViewBuildingCity,  (double) 0);
-			AnchorPane.setLeftAnchor(listViewBuildingCity, (double)  0);
-			AnchorPane.setRightAnchor(listViewBuildingCity, (double)  0);
-			buildingCityViewPane.getChildren().add(listViewBuildingCity);
+			buildingCityUiController = buildingCityUiLoader.getController() ;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+		AnchorPane.setBottomAnchor(listViewBuildingCity, (double) 0);
+		AnchorPane.setTopAnchor(listViewBuildingCity,  (double) 0);
+		AnchorPane.setLeftAnchor(listViewBuildingCity, (double)  0);
+		AnchorPane.setRightAnchor(listViewBuildingCity, (double)  0);
+		buildingCityViewPane.getChildren().add(listViewBuildingCity);
+		buildingCityUiController.setMainApp(this);
+		this.buildingCityList = cities.getBuildingByCityId(2);
+		buildingCityUiController.refresh();
 		
 	}
+
+	public List<BuildingCityBean> getBuildingCityList(){return buildingCityList;}
 
 }
